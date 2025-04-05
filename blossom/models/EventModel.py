51c46b66d5e7c810b1f981a80b105e6217026467
Cable The Wizard <cable@cablethewizard.com>
2025-04-04 20:30:59 -0400

    Update class name for model

--- blossom/models/EventModel.py
+++ blossom/models/EventModel.py
@@ -2,7 +2,7 @@ from django.db import models
 from .LocationModel import Location
 from .ShowModel import Show
 
-class Event(models.Model):
+class ShowEvent(models.Model):
     name = models.CharField(max_length=250)
     starttime = models.DateTimeField("start time")
     endtime = models.DateTimeField("end time")
