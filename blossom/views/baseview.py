4f18f08916d62b41f0cf1c1a0ccc02d8c0fafe7e
Cable The Wizard <cable@cablethewizard.com>
2025-04-04 20:32:25 -0400

    Make index show list of active

--- blossom/views/baseview.py
+++ blossom/views/baseview.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
-def index(request):
-    return HttpResponse("Index found")
+from blossom.models.ShowModel import Show
+
+class IndexView(generic.ListView):
+    template_name = "blossom/index.html"
+    context_object_name = "active_show_list"
+
+    def get_queryset(self):
+        return Show.objects.all().filter(active=True)
