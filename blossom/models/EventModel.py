5dbcedd15b17dc0e1126bf368f5067e890045414
Cable The Wizard <cable@cablethewizard.com>
2025-04-06 15:43:47 -0400

    Update models to use dates instead of datetimes

--- blossom/models/EventModel.py
+++ blossom/models/EventModel.py
@@ -1,3 +1,4 @@
+from optparse import check_builtin
 from django.db import models
 from django.forms import ValidationError
 from .LocationModel import Location
@@ -5,11 +6,13 @@ from .ShowModel import Show
 
 class ShowEvent(models.Model):
     name = models.CharField(max_length=250)
-    starttime = models.DateTimeField("start time")
-    endtime = models.DateTimeField("end time")
+    starttime = models.TimeField("Start Time")
+    endtime = models.TimeField("End Time")
     description = models.TextField(blank=True, null=True)
     room = models.ForeignKey(Location, on_delete=models.DO_NOTHING,null=True)
     show = models.ForeignKey(Show, on_delete=models.CASCADE,null=True)
+    date = models.DateField("Event Date")
+
 
     def check_overlap(self, fixed_start, fixed_end, new_start, new_end):
         overlap = False
@@ -19,11 +22,22 @@ class ShowEvent(models.Model):
             overlap = True
         elif new_start <= fixed_start and new_end >= fixed_end: #outer limits
             overlap = True
+    
+    def check_inshow(self, showstart, showend):
+        inshow = True
+        if showstart > self.date:
+            inshow = False
+        elif showend < self.date:
+            inshow = False
+
 
     def clean(self):
         if self.endtime <= self.starttime:
             raise ValidationError('End time must be after start time')
         
+        if self.check_inshow(self.show.startdate, self.show.enddate):
+            raise ValidationError({'date':'Event date not within show date range'})
+
         roomevents = ShowEvent.objects.filter(room=self.room)
         if roomevents.exists():
             for event in roomevents:
--- blossom/models/ShowModel.py
+++ blossom/models/ShowModel.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Show(models.Model):
     name = models.CharField(max_length=250)
-    starttime = models.DateTimeField("start time")
-    endtime = models.DateTimeField("end time")
+    startdate = models.DateField("Start Date")
+    enddate = models.DateField("End Date")
     description = models.TextField(blank=True,null=True)
     active = models.BooleanField(default=False)
