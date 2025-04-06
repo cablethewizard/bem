2a8221cd558784e48a7ef52c520621b6d4d12f7b
Cable The Wizard <cable@cablethewizard.com>
2025-04-06 15:12:34 -0400

    Update model and migrations

--- blossom/models/EventModel.py
+++ blossom/models/EventModel.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import ValidationError
 from .LocationModel import Location
 from .ShowModel import Show
 
@@ -9,3 +10,23 @@ class ShowEvent(models.Model):
     description = models.TextField(blank=True, null=True)
     room = models.ForeignKey(Location, on_delete=models.DO_NOTHING,null=True)
     show = models.ForeignKey(Show, on_delete=models.CASCADE,null=True)
+
+    def check_overlap(self, fixed_start, fixed_end, new_start, new_end):
+        overlap = False
+        if new_start == fixed_end or new_end == fixed_start:    #edge case
+            overlap = False
+        elif (new_start >= fixed_start and new_start <= fixed_end) or (new_end >= fixed_start and new_end <= fixed_end): #inner limits
+            overlap = True
+        elif new_start <= fixed_start and new_end >= fixed_end: #outer limits
+            overlap = True
+
+    def clean(self):
+        if self.endtime <= self.starttime:
+            raise ValidationError('End time must be after start time')
+        
+        roomevents = ShowEvent.objects.filter(room=self.room)
+        if roomevents.exists():
+            for event in roomevents:
+                if self.check_overlap(event.starttime,event.endtime,self.starttime,self.endtime):
+                    raise ValidationError(
+                            'There is an overlap with another event: ' + str(event.day) + ', ' + str(event.start_time) + '-' + str(event.end_time))
